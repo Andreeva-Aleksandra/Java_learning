673ca0ec650f3412ddaa87c3dc566f8314acd545
Andreeva-Aleksandra <88310279+Andreeva-Aleksandra@users.noreply.github.com>
2021-10-15 11:16:18 +0300

    Create HW_2_2.java

---
+++ QaAutomationCourse/JavaMainHW/HW_2_2.java
@@ -0,0 +1,11 @@
+/*
+Вывести/напечатать смайлик (не :), а настоящий смайлик одним символом).
+*/
+
+public class HW_2_2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\uD83D\uDE00");
+    }
+}
