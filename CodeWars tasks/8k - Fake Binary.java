445d5f99fb50861adef9e9cff70b00e708fe1277
Andreeva-Aleksandra <88310279+Andreeva-Aleksandra@users.noreply.github.com>
2021-10-15 10:20:44 +0300

    Update 8k - Fake Binary.java

--- CodeWars tasks/8k - Fake Binary.java
+++ CodeWars tasks/8k - Fake Binary.java
@@ -5,3 +5,20 @@ public class FakeBinary {
     
     }
 }
+
+
+//other solution
+//
+//public class FakeBinary {
+//    public static String fakeBin(String numberString) {
+//        String res = "";
+//        for (int i = 0; i < numberString.length(); i++) {
+//            if (numberString.charAt(i) < '5') {
+//                res += '0';
+//            } else {
+//                res += '1';
+//            }
+//        }
+//        return res;
+//    }
+//}
