4175e84008107c126189cc1cb51980bc54e875c4
Andreeva-Aleksandra <88310279+Andreeva-Aleksandra@users.noreply.github.com>
2021-10-15 11:15:28 +0300

    Create HW_2_1.java

---
+++ QaAutomationCourse/JavaMainHW/HW_2_1.java
@@ -0,0 +1,24 @@
+/*
+Необходимо создать целочисленные переменные a и b, 
+присвоить произвольные значения переменным на ваш выбор 
+и вывести результаты следующих операций с этими переменными: 
+сложение, умножение, вычитание, деление и остаток от деления. 
+Так же сделать проверку на четность этих переменных и вывести результат.
+*/
+
+public class HW_2_1 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        System.out.println(a % 2);
+        System.out.println(b % 2);
+    }
+}
