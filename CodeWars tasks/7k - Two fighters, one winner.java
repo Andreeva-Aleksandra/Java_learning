8d91cb4400a5275dc2511418b540e4e9b623463a
Andreeva-Aleksandra <88310279+Andreeva-Aleksandra@users.noreply.github.com>
2021-10-16 09:40:03 +0300

    Create 7k - Two fighters, one winner.java

---
+++ CodeWars tasks/7k - Two fighters, one winner.java
@@ -0,0 +1,22 @@
+public class Kata {
+  public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+    
+    while (true) {
+      fighter1.health -= fighter2.damagePerAttack;
+      fighter2.health -= fighter1.damagePerAttack;
+
+      if (fighter1.health <= 0 && fighter2.health > 0) {
+        return fighter2.name;
+      }
+      if (fighter2.health <= 0 && fighter1.health > 0) {
+        return fighter1.name;
+      }
+    
+      if (fighter1.health <= 0 && fighter2.health <= 0) {
+        break;
+      }
+    }
+    
+    return firstAttacker;
+  }
+}
