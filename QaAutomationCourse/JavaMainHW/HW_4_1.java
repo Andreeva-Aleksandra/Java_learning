d9727e493a5f81a46a4014e89497cb3fd9da59ba
Andreeva-Aleksandra <88310279+Andreeva-Aleksandra@users.noreply.github.com>
2021-10-17 16:34:35 +0300

    Create HW_4_1.java

---
+++ QaAutomationCourse/JavaMainHW/HW_4_1.java
@@ -0,0 +1,12 @@
+/*
+Необходимо вывести числа от 0 до 15.
+ */
+
+public class HW_4_1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i = i + 1) {
+            System.out.println(i);
+        }
+    }
+}
