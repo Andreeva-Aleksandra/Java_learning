7b2e19fe0ea6caa39fec699280ca2531d3bd9229
Andreeva-Aleksandra <88310279+Andreeva-Aleksandra@users.noreply.github.com>
2021-10-17 16:35:04 +0300

    Create HW_4_2.java

---
+++ QaAutomationCourse/JavaMainHW/HW_4_2.java
@@ -0,0 +1,13 @@
+/*
+Необходимо вывньше 10000, вывести результат возведения
+в степень.
+ */
+
+public class HW_4_2 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 10000; i = i * 5) {
+            System.out.println(i);
+        }
+    }
+}
