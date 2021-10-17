aa98f671b6e30c0d8e643bc5d4c8c7f8f9541c3b
Andreeva-Aleksandra <88310279+Andreeva-Aleksandra@users.noreply.github.com>
2021-10-17 16:31:53 +0300

    Create HW_3_2.java

---
+++ QaAutomationCourse/JavaMainHW/HW_3_2.java
@@ -0,0 +1,20 @@
+/*
+Необходимо создать две целочисленные переменные (a, b), присвоить произвольные значения переменным на ваш выбор и вывести следующие строки:
+-maybe a and b are even - если сумма переменных четная
+-some variable is odd - если сумма переменных нечетная
+ */
+
+public class HW_3_2 {
+
+    public static void main(String[] args) {
+
+        int a = 9;
+        int b = 9;
+
+        if ((a + b) % 2 == 0) {
+            System.out.println("maybe a and b are even");
+        } else {
+            System.out.println("some variables are odd");
+        }
+    }
+}
